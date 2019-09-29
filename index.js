5a0e9b5f8368739fa16c69e7cf22eaa678c13b73
saja amer <sajaamer86@gmail.com>
2019-09-29 10:29:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arr){
+  let search=arr.find(arr=>arr.result==="W");
+ 
+  if (search===undefined){
+    return search;
+  }else{
+    return search.year;
+  }
+  return arr;
+}
